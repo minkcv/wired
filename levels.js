78964001f0f1940f4040dce82d9b0eb685c58e74
Will Smith <minkcv@gmail.com>
2018-11-12 14:04:50 -0700

    add lenses

--- levels.js
+++ levels.js
@@ -375,7 +375,7 @@ function loadLevel6() {
     TH.addSpinningCylinderPart(499, 10, 0, 149, 75, 0.01, 0.392);
     TH.addSpinningCylinderPart(499, 10, 0, 141, 75, 0.01, 0.392);
     var floorPoints = [{x:0,y:-1,z:0},{x:-150,y:0,z:0},{x:-147.11849999999998,y:0,z:-29.2635},{x:-138.582,y:0,z:-57.402},{x:-124.7205,y:0,z:-83.33550000000001},{x:-106.0665,y:0,z:-106.0665},{x:-83.33550000000001,y:0,z:-124.7205},{x:-57.402,y:0,z:-138.582},{x:-29.2635,y:0,z:-147.11849999999998},{x:0,y:0,z:-150},{x:29.2635,y:0,z:-147.11849999999998},{x:57.402,y:0,z:-138.582},{x:83.33550000000001,y:0,z:-124.7205},{x:106.0665,y:0,z:-106.0665},{x:124.7205,y:0,z:-83.33550000000001},{x:138.582,y:0,z:-57.402},{x:147.11849999999998,y:0,z:-29.2635},{x:150,y:0,z:0},{x:147.11849999999998,y:0,z:29.2635},{x:138.582,y:0,z:57.402},{x:124.7205,y:0,z:83.33550000000001},{x:106.0665,y:0,z:106.0665},{x:83.33550000000001,y:0,z:124.7205},{x:57.402,y:0,z:138.582},{x:29.2635,y:0,z:147.11849999999998},{x:0,y:0,z:150},{x:-29.2635,y:0,z:147.11849999999998},{x:-57.402,y:0,z:138.582},{x:-83.33550000000001,y:0,z:124.7205},{x:-106.0665,y:0,z:106.0665},{x:-124.7205,y:0,z:83.33550000000001},{x:-138.582,y:0,z:57.402},{x:-147.11849999999998,y:0,z:29.2635}]
-    TH.addShape(499, -26, 0, floorPoints, 0);
+    TH.addShape(499, -27, 0, floorPoints, 0);
     TH.addSpinningModel(499, 10, 0, models.spinroom, 0.01, 0.15, 'y', 0);
     TH.addSpinningModel(499, 10, 0, models.spinroomthreshhold, 0.01, 0.15, 'y', 0);
     TH.addSpinningModel(499, 10, 0, models.spinroom, 0.01, 0.14, 'y', 0);
@@ -410,11 +410,13 @@ function loadLevel6() {
     addSpinningObject(460, -15, 350, models.gear, -0.01, 0.1, 0, 'z', 0);
     
     // Center gears
-    addSpinningObject(490, -25, 0, models.gearflat, 0.01, 0.1, 15, 'y', 0);
-    addSpinningObject(490, -25, 25, models.gearflat, -0.01, 0.1, 15, 'y', 0);
-    addSpinningObject(490, -25, -25, models.gearflat, -0.01, 0.1, 15, 'y', 0);
-    addSpinningObject(515, -25, 0, models.gearflat, -0.01, 0.1, 15, 'y', 0);
-    addSpinningObject(465, -25, 0, models.gearflat, -0.01, 0.1, 15, 'y', 0);
+    addSpinningObject(490, -29, 0, models.gearflat, 0.01, 0.1, 15, 'y', 0);
+    addSpinningObject(490, -29, 25, models.gearflat, -0.01, 0.1, 15, 'y', 0);
+    addSpinningObject(490, -29, -25, models.gearflat, -0.01, 0.1, 15, 'y', 0);
+    addSpinningObject(515, -29, 0, models.gearflat, -0.01, 0.1, 15, 'y', 0);
+    addSpinningObject(465, -29, 0, models.gearflat, -0.01, 0.1, 15, 'y', 0);
+
+    addSpinningObject(490, -27, 0, models.lenses, 0.01, 0.1, 15, 'y', 0);
 
     // Right tunnel
     for (let index = 0; index < 2; index++) {
